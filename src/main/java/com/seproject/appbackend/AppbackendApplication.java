0dfd18cbfd4031dee4727861770145383bc469fa
Cem <cem_c@hotmail.de>
2024-04-14 14:23:53 +0200

    sdsds

---
+++ src/main/java/com/seproject/appbackend/AppbackendApplication.java
@@ -0,0 +1,13 @@
+package com.seproject.appbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seproject/appbackend/AppbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seproject.appbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
